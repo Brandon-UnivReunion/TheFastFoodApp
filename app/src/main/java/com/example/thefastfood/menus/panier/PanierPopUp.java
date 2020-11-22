c0eab247649e089f9ca2ac97efc30f2e16f1b39a
BrandonCKF <60539536+Brandon-UnivReunion@users.noreply.github.com>
2020-11-22 17:33:53 +0400

    Ajout du prix total du panier

--- app/src/main/java/com/example/thefastfood/menus/panier/PanierPopUp.java
+++ app/src/main/java/com/example/thefastfood/menus/panier/PanierPopUp.java
@@ -15,6 +15,7 @@ import com.example.thefastfood.R;
 import com.example.thefastfood.menus.adapter.PanierAdapter;
 import com.example.thefastfood.menus.dataBase.DatabaseManager;
 import com.example.thefastfood.menus.item.Offre;
+import com.example.thefastfood.menus.listOffres.ListOffres;
 
 import java.util.ArrayList;
 
@@ -22,21 +23,39 @@ public class PanierPopUp extends Dialog {
     private ListView listView;
     private TextView prixTotal;
     private Button valideButton, videButton, annuleButton;
+    private PanierPopUp panierPopUp;
 
     public PanierPopUp(Activity context,  DatabaseManager dm) {
         super(context, R.style.Theme_AppCompat_DayNight_Dialog);
         setContentView(R.layout.popup_panier);
 
+        panierPopUp = this;
+
         listView = findViewById(R.id.panierListview);
+
         prixTotal = findViewById(R.id.panierPrixTotal);
+        ArrayList<Offre> listOffres = dm.readPanier();
+        float total = 0;
+        for (Offre offre: listOffres) {
+            total += offre.getPrix();
+        }
+        prixTotal.setText(String.valueOf(total));
+
         valideButton = findViewById(R.id.panierValider);
-        videButton = findViewById(R.id.panierAnnuler);
+        videButton = findViewById(R.id.panierVider);
+
+        annuleButton = findViewById(R.id.panierAnnuler);
+        annuleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                panierPopUp.dismiss();
+            }
+        });
 //        Log.d("PANIERPOPUP", listView.toString());
         PanierAdapter panierAdapter = new PanierAdapter(context, dm);
         listView.setAdapter(panierAdapter);
         panierAdapter.notifyDataSetChanged();
     }
-
 
 
 
