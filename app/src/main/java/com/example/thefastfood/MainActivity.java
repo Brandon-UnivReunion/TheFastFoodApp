a89204fb1f8d8b8f2ac9d063fca37593faa6fde9
Brandon <brandon99.univ@gmail.com>
2020-11-23 21:21:53 +0400

    Menu en fragment

--- app/src/main/java/com/example/thefastfood/MainActivity.java
+++ app/src/main/java/com/example/thefastfood/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.thefastfood;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.thefastfood.menus.activities.MainMenuActivity;
 import com.example.thefastfood.menus.dataBase.DatabaseManager;
+import com.example.thefastfood.menus.fragments.AfficheMenuFragment;
 import com.example.thefastfood.menus.panier.PanierPopUp;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
 
 
@@ -26,9 +33,52 @@ public class MainActivity extends AppCompatActivity {
 //        finish();
     }
 
-    public void panierClick(View view){
-
-        PanierPopUp panierPopUp = new PanierPopUp(this, new DatabaseManager(this));
-        panierPopUp.show();
+    public void menuClick(View view){
+        displayFragment(new AfficheMenuFragment());
     }
+
+    public void panierClick(View view){
+        final String SHARED_PREFERENCES_NAME = "DB";
+        DatabaseManager databaseManager = new DatabaseManager(this);
+
+        // Persistence pour savoir si le panier à déjà été utilisé
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE);
+
+
+        if(sharedPreferences.getBoolean("panier", false) && databaseManager.readPanier().size() != 0) {
+            PanierPopUp panierPopUp = new PanierPopUp(this, databaseManager);
+            panierPopUp.show();
+        }else{
+            Toast.makeText(this, "Le panier est vide !", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Affiche le fragment
+     * @param fragment
+     */
+    private void displayFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment, fragment, "currentFragment");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    /**
+     * Gestion des evenements
+     * @param ev
+     * @return
+     */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        // TODO implement fragment gesture listener
+//        myGestureDetectorListener.onTouchEvent(ev);
+        Log.d("dispatch2", "dispatchTouchEvent");
+        AfficheMenuFragment afficheMenuFragment = (AfficheMenuFragment) getSupportFragmentManager().findFragmentByTag("currentFragment");
+        Log.d("dispatch2", String.valueOf(afficheMenuFragment));
+        if(afficheMenuFragment != null && afficheMenuFragment instanceof AfficheMenuFragment)
+            afficheMenuFragment.actionDispatchTouchEvent(ev);
+        return super.dispatchTouchEvent(ev);
+    }
+
 }
